b1a63ed6aad208a5d234211e9c055e78469c182e
R1AIN <166348375+R1AIN@users.noreply.github.com>
2024-06-26 09:15:57 +0545

    Add files via upload

---
+++ Ken.js
@@ -0,0 +1,61 @@
+//Variables
+//var result = 90;
+//console.log(result);
+//Data Types
+//number
+// const x = 90;
+// console.log(x);
+// console.log(typeof x);
+// String;
+// const output = "Emma Limbu";
+// console.log(output);
+// console.log(typeof output);
+//Boolean
+// const isMarried = false;
+// console.log(isMarried);
+// console.log(typeof isMarried);
+// undefined
+// let a;
+// console.log(a);
+// console.log(typeof a);
+// null
+// const x = null;
+// console.log(x);
+// console.log(typeof x);
+// BigInt;
+// const x = 6526574n;
+// console.log(x);
+// console.log(typeof x);
+// Symbol;
+// const result = Symbol("Who are you");
+// console.log(result);
+// console.log(typeof result);
+// Object types
+// const Object = {
+// 	fName: "Emma Limbu",
+// 	age: 19,
+// 	isMarried: true,
+// 	occupation: "Coder",
+// 	salary: 10000,
+// };
+// console.log(Object);
+// console.log(typeof Object);
+
+// const arr = [3, 4, 5, 6, 7, 8];
+// console.log(arr);
+// console.log(typeof arr);
+
+// const date = new Date();
+// console.log(date);
+// console.log(typeof date);
+// Type Conversion
+// const x = "8.98";
+// console.log(x);
+// console.log(typeof x);
+
+const result = 1 + "1";
+console.log(result);
+console.log(typeof result);
+// const output = 8 - "4";
+// console.log(output);
+// console.log(typeof output);
